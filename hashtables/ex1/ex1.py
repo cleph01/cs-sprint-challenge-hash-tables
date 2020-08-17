c605221cc2e97ed1feecc22ac94defa2bd82062a
Charlie <charles.j.montoya@gmail.com>
2020-08-16 21:23:07 -0400

    ex1 passing 3/4

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,42 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
 
-    return None
+    found = False
+
+    for idx, value in enumerate(weights):
+        
+        target = limit - value
+
+        if target in dict:
+
+            found == True
+
+            if target > value:
+
+                return (dict.get(target), idx)
+            
+            elif target < value:
+
+                return (idx, dict.get(target))
+
+            else:
+
+                return (idx, dict.get(target))
+    
+        elif target not in dict and found == False:
+
+            dict[value] = idx
+
+        else: 
+
+            return None
+
+
+
+    
+
+weights_2 = [4, 4]
+
+get_indices_of_item_weights(weights_2, 2, 8)
